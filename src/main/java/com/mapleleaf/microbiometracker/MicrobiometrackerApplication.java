e5de15a5ee03006a856fbf2c35ab505962657f7e
Luca <lucadg27@hotmail.it>
2024-09-29 16:43:56 +0200

    first commit

---
+++ src/main/java/com/mapleleaf/microbiometracker/MicrobiometrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mapleleaf.microbiometracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobiometrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobiometrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mapleleaf/microbiometracker/MicrobiometrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapleleaf.microbiometracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobiometrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
